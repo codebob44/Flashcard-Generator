1d3d1f7b4618862458c8711737665916c7fd374c
Bob Carroll <avpbob@gmail.com>
2018-01-08 01:39:21 -0500

    finished ClozeCard and fixed fullText error

--- BasicCard.js
+++ BasicCard.js
@@ -1,20 +1,21 @@
-// This file should define a Node module that exports a constructor for creating basic flashcards, e.g.:
-module.exports = BasicCard;
-
-// The constructor should accept two arguments: front and back.
-// The constructed object should have a front property that contains the text on the front of the card.
-// The constructed object should have a back property that contains the text on the back of the card.
 function BasicCard(front, back) {
-	this.front = front;
-	this.back = back;
+
+  // Scope-safe constructor. This code allows us to optionally create BasicCards without using the new keyword
+  if (!(this instanceof BasicCard)) {
+    return new BasicCard(front, back);
+  }
+
+  this.front = front;
+  this.back = back;
 }
 
-// var firstPresident = new BasicCard(
-//     "Who was the first president of the United States?", "George Washington");
+module.exports = BasicCard;
 
-// // "Who was the first president of the United States?"
-// console.log(firstPresident.front); 
+var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
 
-// // "George Washington"
-// console.log(firstPresident.back); 
 
+console.log();
+console.log("----- Basic Card -----");
+console.log(firstPresident.front);
+console.log(firstPresident.back);
+console.log("----------------------");
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,18 +1,58 @@
-var ClozeCard = function(text, cloze){
-	if(this instanceof ClozeCard){
-		this.fullText = text;
-		this.cloze = cloze;
+// text is the full text of the cloze card
+// clozeDeletion is the portion of the text we want to hide
+function ClozeCard(text, clozeDeletion) {
 
-		if(this.fullText.includes(this.cloze)){
-			this.partial = this.fullText.replace(this.cloze, "...");
-		}
-		else {
-			console.log(this.cloze + this.fullText + ".");
-		}
-	}
-	else {
-		return new ClozeCard(text, cloze);
-	}
+  // Scope-safe constructor. This code allows us to optionally create ClozeCards without using the new keyword
+  if (!(this instanceof ClozeCard)) {
+    return new ClozeCard(text, clozeDeletion);
+  }
+
+  // Holds [start, end] positions for clozeDeletion.
+  var clozePostions = clozeDelete(text, clozeDeletion);
+
+  // Builds "partial" text--e.g., input text without cloze deletion
+  this.partial = getPartial(text, clozePostions);
+
+  // Saves the cloze deleted text for later display
+  this.cloze = text.slice(clozePostions[0], clozePostions[1]);
+
+  // Reference to the full text
+  this.fullText = text;
+
+  // "Pseudo-private" functions, purposefully not on the prototype
+  function getPartial(text, clozePostions) {
+
+    // Find where cloze deletion begins in text
+    var start = text.slice(0, clozePostions[0]);
+    // Remove cloze range
+    var end = text.slice(clozePostions[1], text.length);
+
+    // Return ellipsized version
+    return start + "..." + end;
+  }
+
+  function clozeDelete(text, clozeDeletion) {
+    var start = text.indexOf(clozeDeletion);
+
+    if (start !== -1) {
+      return [start, start + clozeDeletion.length];
+    }
+    throw new Error("Cloze deletion not found in input text.");
+  }
+}
+
+ClozeCard.prototype.displayCard = function displayCard() {
+  return this.partial.replace(/\.\.\./, "'" + this.cloze + "'");
 };
 
 module.exports = ClozeCard;
+
+
+var firstPresidentCloze = new ClozeCard("George Washington was the first president of the United States.", "George Washington");
+
+console.log();
+console.log("----- Cloze Card -----");
+console.log(firstPresidentCloze.cloze);
+console.log(firstPresidentCloze.partial);
+console.log(firstPresidentCloze.fullText);
+console.log("----------------------");
